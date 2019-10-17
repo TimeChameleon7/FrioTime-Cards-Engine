c8dc9ebecb526deb23a9ba9748398c81a1f11741
kelledar <creepersuit7@gmail.com>
2019-10-17 13:21:25 -0500

    Card documentation

--- src/com/ft/ce/core/Card.java
+++ src/com/ft/ce/core/Card.java
@@ -1,9 +1,23 @@
 package com.ft.ce.core;
 
+/**
+ * Represents a standard playing card.
+ *
+ * Note: Joker cards have a {@code null} {@linkplain Suite},
+ * references to Joker cards should check for the {@linkplain Name}
+ * of the card.
+ */
 public class Card {
     public final Name name;
     public final Suite suite;
 
+    /**
+     * Creates a card from the input {@linkplain Name} and {@linkplain Suite}.
+     * Note: If the Name of the card is {@code JOKER} then Suite will be set to null.
+     *
+     * @param name The Name of the card.
+     * @param suite The Suite of the card.
+     */
     public Card(Name name, Suite suite) {
         this.name = name;
         if (name != Name.JOKER) {
@@ -13,10 +27,21 @@ public class Card {
         }
     }
 
+    /**
+     * Creates a card from the byte input, using the bit masks from {@linkplain Name} & {@linkplain Suite}.
+     *
+     * @param b The byte to be converted to a Card.
+     */
     public Card(byte b) {
         this(Name.getInstance(b), Suite.getInstance(b));
     }
 
+    /**
+     * Returns a byte[] containing the byte representation of {@code this}.
+     *
+     * Note: The byte[] will always have a length of 1.
+     * @return a byte[] containing the byte representation of {@code this}.
+     */
     public byte[] toBytes() {
         if (name == Name.JOKER) {
             return new byte[]{name.mask};
@@ -25,10 +50,22 @@ public class Card {
         }
     }
 
+    /**
+     * Returns {@code true} if {@code this} Card's {@linkplain Name} is equal to the input Name.
+     *
+     * @param name Name to check {@code this} Card's Name against.
+     * @return {@code true} if {@code this} Card's Name is equal to the input Name.
+     */
     public boolean is(Name name) {
         return this.name == name;
     }
 
+    /**
+     * Returns {@code true} if {@code this} Card's {@linkplain Suite} is equal to the input Suite.
+     *
+     * @param suite Suite to check {@code this} Card's Suite against.
+     * @return {@code true} if {@code this} Card's Suite is equal to the input Suite.
+     */
     public boolean is(Suite suite) {
         return this.suite == suite;
     }
